dd21b0832684c9d8b44780eea5f8acd9a0272fc6
swapnaamara <swapnaamara59@gmail.com>
2026-09-25 19:12:10 +0530

    Refactor code for counting even and odd numbers

--- Count Even and Odd numbers in Array.py
+++ Count Even and Odd numbers in Array.py
@@ -1,5 +1,6 @@
 n = int(input("Enter size: "))
 arr = []
+
 for i in range(n):
     arr.append(int(input(f"Enter value {i+1}: ")))
 
@@ -7,6 +8,7 @@ even = 0
 odd = 0
 
 for x in arr:
+    
     if x % 2 == 0:
         even += 1
     else:
